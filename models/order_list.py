dfa38b618f9516368ffe714e20c0ae86ac64f3da
Ross Brown <rossbrown22@hotmail.co.uk>
2021-11-24 16:53:42 +0000

    flask_model_labhw

---
+++ models/order_list.py
@@ -0,0 +1,7 @@
+from re import A
+from models.order import *
+
+order1 = Order("Ross","24/11/2021" , 1, "Vanilla")
+order2 = Order("Sky", "20/11/2021", 3, "Jam-Filled")
+order3 = Order("Peter", "18/11/2021", 2, "Chocolate")
+orders = [order1, order2, order3]
